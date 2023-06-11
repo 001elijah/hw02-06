0288342eb772832a043126f645cba37e081b2635
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-06-11 12:37:51 +0300

    add email verification

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require('./HttpError');
 const ctrlrWrapper = require('./ctrlrWrapper');
 const handleMongooseError = require('./handleMongooseError');
+const sendEmail = require('./sendEmail');
 
 module.exports = {
     HttpError,
     ctrlrWrapper,
-    handleMongooseError
+    handleMongooseError,
+    sendEmail
 };
